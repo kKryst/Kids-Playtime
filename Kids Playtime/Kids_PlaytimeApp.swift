5a6eb56d4d2262661f5bb2dff030c5c0f54156f7
kKryst <92850700+kKryst@users.noreply.github.com>
2024-01-14 13:57:19 +0100

    Initial Commit

---
+++ Kids Playtime/Kids_PlaytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_PlaytimeApp.swift
+//  Kids Playtime
+//
+//  Created by Krystian Konieczko on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kids_PlaytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
